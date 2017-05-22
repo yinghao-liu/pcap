b859eb7b752b9372f9af27b233f4536b98be122c
yinghao-liu <yinghao_liu@163.com>
2017-05-22 13:21:34 +0800

    ip_checksum ok

--- main.c
+++ main.c
@@ -66,7 +66,69 @@ void mac_to_hex(uint8_t* hex, const char* mac)
 {
 	sscanf(mac, "%x:%x:%x:%x:%x:%x", &hex[0],&hex[1],&hex[2],&hex[3],&hex[4],&hex[5]);
 }
+/********************************ip_checksum*******************************************/
+void ip_checksum(struct iphdr *ip_sp)
+{
+	uint8_t   tick;
+	uint32_t  sum=0;
+	uint16_t* tmp=NULL;
 
+	ip_sp->check = 0;
+	tmp = (uint16_t*)ip_sp;
+
+	for (tick=0; tick<ip_sp->ihl*2; tick++){
+		sum += tmp[tick]; 	
+	}
+	while (sum > 0xffff){
+		sum = (sum & 0xffff) +(sum >> 16);
+	}
+	sum = ~sum & 0xffff;
+	ip_sp->check = sum;/*no need htons()*/
+}
+/********************************tcp_checksum*******************************************/
+void tcp_checksum(struct iphdr* ip_sp, struct tcphdr* tcp_sp)
+{
+	uint8_t   tick;
+	uint32_t  sum=0;
+	uint16_t* tmp=NULL;
+	uint16_t  tcp_len=0;
+
+
+	tcp_sp->th_sum = 0;
+	tmp = (uint16_t*)(&ip_sp->saddr);
+	for (tick=0; tick<4; tick++){
+		sum += tmp[tick];
+		printf("%x\n", tmp[tick]);
+	}
+	sum += ip_sp->protocol<<8;
+
+	tcp_len = ntohs(ip_sp->tot_len) - ((char*)tcp_sp-(char*)ip_sp) ; /*tcp length, include tcp head and data*/
+	
+	sum += htons(6+tcp_len);
+	
+
+	printf("tcplen is %d\n",tcp_len);
+	if (tcp_len%2 != 0){
+		tcp_len += 1;
+		((char*)tcp_sp)[tcp_len] = 0; /*here may segment fault*/
+	}
+
+	tcp_len = tcp_len/2; 
+	printf("tcplen is %d\n",tcp_len);
+	tmp = (uint16_t*)tcp_sp;
+	for(tick=0; tick<tcp_len; tick++){
+		sum += tmp[tick];	
+		printf("%x\n", tmp[tick]);
+	}
+
+	while (sum > 0xffff){
+		sum = (sum & 0xffff) +(sum >> 16);
+	}
+	sum = ~sum;
+	tcp_sp->th_sum = sum;
+}
+
+/************************** send_packet *************************************/
 int send_packet(void)
 {
 	uint16_t buff_len;
@@ -76,59 +138,70 @@ int send_packet(void)
 	struct tcphdr *tcp_sp = NULL;
 	uint8_t *vlan_sp = NULL; 
 	uint8_t *beyond_tcp = NULL; 
-	char tmp[]={2,4,5,0xb4,4,2,8,0x0a,1,0x49,0x77,0x9a,0,0,0,0,1,3,3,7};	
+	char tmp[]={2,4,5,0xb4,4,2,8,0x0a,0x75,0x39,0xe9,0x92,0,0,0,0,1,3,3,7};	
+	//char tmp[]={2,4,5,0xb4,1,3,3,2,1,1,4,2};	
 	pcap_t *eth_out=NULL;
 /****************************** ether layer *************************************/
 	ether_sp = (struct ether_header*)buff;
 	mac_to_hex(ether_sp->ether_dhost, "00:50:56:eb:b0:73");
 	mac_to_hex(ether_sp->ether_shost, "00:0c:29:bc:0c:f5");
-	//ether_sp->ether_type = htons(ETHERTYPE_IP);
-	ether_sp->ether_type = htons(ETHERTYPE_VLAN);
+	ether_sp->ether_type = htons(ETHERTYPE_IP);
+	//ether_sp->ether_type = htons(ETHERTYPE_VLAN);
 
 /****************************** vlan layer *************************************/
-	vlan_sp = (uint8_t*)(ether_sp + 1);
+/*	vlan_sp = (uint8_t*)(ether_sp + 1);
 	vlan_sp[0]=0;
 	vlan_sp[1]=2;
 	vlan_sp[2]=8;
 	vlan_sp[3]=0;
- 
+ */
 /****************************** ip layer *************************************/
-	ip_sp = (struct iphdr*)(vlan_sp+4);
+	//ip_sp = (struct iphdr*)(vlan_sp+4);
+	ip_sp = (struct iphdr*)(ether_sp+1);
 	ip_sp->version = 4;
 	ip_sp->ihl = 5;//length 5*4 =20 byte
 	ip_sp->tos = 0;
-	ip_sp->tot_len = htons(60); //reassign below
-	ip_sp->id = htons(0x1a6c);//maybe wrong
+	ip_sp->tot_len = 0; //reassign below
+	ip_sp->id = htons(0xe1d1);//maybe wrong
 	ip_sp->ttl= 64;
 	ip_sp->protocol = IPPROTO_TCP;
 	ip_sp->frag_off = htons(0x4000);
-	ip_sp->check = htons(0x383d);
 	ip_sp->saddr = inet_addr("192.168.79.135");
-	ip_sp->daddr = inet_addr("223.6.248.220");
+	ip_sp->daddr = inet_addr("42.121.252.58");
 
 /****************************** tcp layer *************************************/
 	tcp_sp = (struct tcphdr*)(ip_sp + 1);
-	tcp_sp->th_sport = htons(50604);
+	tcp_sp->th_sport = htons(39312);
 	tcp_sp->th_dport = htons(80);
-	tcp_sp->th_seq = htonl(0xba3f6df6);
+	tcp_sp->th_seq = htonl(0x3940e041);
 	tcp_sp->th_ack = htonl(0);
-	tcp_sp->th_off = 0x0a;
+	tcp_sp->th_off = 10;
 	tcp_sp->th_flags = TH_SYN;
 	tcp_sp->th_win = htons(29200);
-	tcp_sp->th_sum = htons(0xe841);
 
 
 /****************************** tcp2 layer *************************************/
+
 	beyond_tcp = (uint8_t*)(tcp_sp+1);
 	memcpy(beyond_tcp, tmp, sizeof (tmp));
 	buff_len = (char*)beyond_tcp - (char*)ether_sp + sizeof (tmp);
+
+	ip_sp->tot_len = htons(buff_len - sizeof (struct ether_header));
+	ip_checksum(ip_sp);
+	tcp_checksum(ip_sp,tcp_sp);
+
+
+
 	print_raw_data(buff,buff_len);
+/*	
 	eth_out = open_pcap("ens33");
 	if (eth_out == NULL){
 		return -1;
 	}
 
 	pcap_inject(eth_out, buff, buff_len);
+	pcap_close(eth_out);
+*/
 	return 0;
 }
 
